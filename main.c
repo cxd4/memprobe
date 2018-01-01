66bc9329a6965aec1d8b14c2be41c35beb73ec4a
Cat vs. Internet <cxd4@uniplex.cat-vs-internet.gov>
2018-01-01 17:06:22 -0500

    initial working source

---
+++ main.c
@@ -0,0 +1,67 @@
+#include <stdlib.h>
+#include <time.h>
+
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char* argv[])
+{
+    char* command;
+    unsigned long addr_start, addr_end, swap_temporary;
+    int shell_available;
+    clock_t t1, t2;
+    register unsigned long i;
+
+    if (argc < 2) {
+        puts("Syntax:");
+        printf("\t%s [starting address] [ending address]\n", argv[0]);
+        printf("\t%s [ending address] [starting address]\n", argv[0]);
+        printf("\t%s [single address]\n", argv[0]);
+        return 0;
+    }
+    if (argv[1][0] != '-') { /* branch */
+    } else {
+        unsigned char memory_byte;
+
+        addr_start = strtoul(argv[2], NULL, 16);
+        memory_byte = *(unsigned char *)(addr_start);
+        printf("0x%02X (%c)", memory_byte, memory_byte);
+        return 0;
+    }
+
+    addr_start = strtoul(argv[1], NULL, 0);
+    addr_end   = addr_start;
+    if (argc >= 3)
+        addr_end = strtoul(argv[2], NULL, 0);
+    if (addr_end < addr_start) {
+        swap_temporary = addr_start;
+        addr_start = addr_end;
+        addr_end = swap_temporary;
+    }
+    command = malloc(
+        strlen(argv[0])
+      + strlen(" -? ")
+      + strlen("0x")
+      + 2*sizeof(void *)
+      + sizeof('\0')
+    );
+
+    shell_available = system(NULL);
+    if (!shell_available) {
+        fprintf(stderr, "No command shell available.\n");
+        return 1;
+    }
+
+    t1 = clock();
+    for (i = 0; addr_start + i <= addr_end; i++) {
+        printf("RAM[0x%lX]:  ", addr_start + i);
+        sprintf(command, "%s -- 0x%lX", argv[0], addr_start + i);
+        fflush(NULL);
+        system(command);
+        putchar('\n');
+    }
+    t2 = clock();
+    putchar('\n');
+    printf("Time taken:  %f seconds.\n", (float)(t2 - t1) / CLOCKS_PER_SEC);
+    return 0;
+}
