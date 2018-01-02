84aeac92be1fee5bcf6adad4cf0a4b0c3aa47ffe
Iconoclast <cxd4@cat-vs-internet.gov>
2018-01-01 19:25:03 -0500

    ported to Windows

--- main.c
+++ main.c
@@ -7,10 +7,10 @@
 int main(int argc, char* argv[])
 {
     char* command;
-    unsigned long addr_start, addr_end, swap_temporary;
     int shell_available;
     clock_t t1, t2;
-    register unsigned long i;
+    size_t addr_start, addr_end, swap_temporary;
+    register size_t i;
 
     if (argc < 2) {
         puts("Syntax:");
@@ -23,16 +23,16 @@ int main(int argc, char* argv[])
     } else {
         unsigned char memory_byte;
 
-        addr_start = strtoul(argv[2], NULL, 16);
+        addr_start = (size_t)strtoul(argv[2], NULL, 16);
         memory_byte = *(unsigned char *)(addr_start);
         printf("0x%02X (%c)", memory_byte, memory_byte);
         return 0;
     }
 
-    addr_start = strtoul(argv[1], NULL, 0);
+    addr_start = (size_t)strtoul(argv[1], NULL, 0);
     addr_end   = addr_start;
     if (argc >= 3)
-        addr_end = strtoul(argv[2], NULL, 0);
+        addr_end = (size_t)strtoul(argv[2], NULL, 0);
     if (addr_end < addr_start) {
         swap_temporary = addr_start;
         addr_start = addr_end;
