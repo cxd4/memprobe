04f1eacc90cca05fe9ce68e9042dfe75c6e94c6f
Iconoclast <cxd4@uniplex.cat-vs-internet.gov>
2018-01-04 01:15:14 -0500

    Use ANSI standard signal handlers to diagnose SIGSEGV.

--- main.c
+++ main.c
@@ -4,6 +4,14 @@
 #include <stdio.h>
 #include <string.h>
 
+#include <signal.h>
+
+void exception_handler(int parameter)
+{
+    printf("Encountered exception signal %i.\n", parameter);
+    return;
+}
+
 int main(int argc, char* argv[])
 {
     char* command;
@@ -19,13 +27,24 @@ int main(int argc, char* argv[])
         printf("\t%s [single address]\n", argv[0]);
         return 0;
     }
+    signal(SIGINT, SIG_IGN);
+    signal(SIGTERM, SIG_DFL);
+    signal(SIGILL, exception_handler);
+    signal(SIGABRT, exception_handler);
+    signal(SIGFPE, exception_handler);
+    signal(SIGSEGV, exception_handler);
+#if 0
+    signal(SIGQUIT, SIG_IGN);
+    signal(SIGCHLD, exception_handler);
+#endif
+
     if (argv[1][0] != '-') { /* branch */
     } else {
         unsigned char memory_byte;
 
         addr_start = (size_t)strtoul(argv[2], NULL, 16);
         memory_byte = *(unsigned char *)(addr_start);
-        printf("0x%02X (%c)", memory_byte, memory_byte);
+        printf("0x%02X (%c)\n", memory_byte, memory_byte);
         return 0;
     }
 
@@ -58,7 +77,6 @@ int main(int argc, char* argv[])
         sprintf(command, "%s -- 0x%lX", argv[0], addr_start + i);
         fflush(NULL);
         system(command);
-        putchar('\n');
     }
     t2 = clock();
     putchar('\n');
